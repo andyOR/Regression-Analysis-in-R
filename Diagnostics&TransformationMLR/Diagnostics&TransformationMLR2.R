33cccb6b3188846cdc85c0f01757a83e27187294
andyOR <aniketskpl19@gmail.com>
2017-07-23 23:01:44 -0400

    Added Dignostics and Transformation for MLR2

---
+++ Diagnostics&TransformationMLR/Diagnostics&TransformationMLR2.R
@@ -0,0 +1,3 @@
+## Tranform both X and Y
+# When some, or all, of the predictors and the response are highly skewed and
+# transformations of these variables are desirable, there are 2 alternative approaches
